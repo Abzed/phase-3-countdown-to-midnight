2c37b9a4bbe3646d97a9d5651a7f904e9873cb94
Abzed <ibraabzed@gmail.com>
2022-07-18 18:14:15 +0300

    commit tree 3549e9445cc9f4e0283cd8411feb05b272f4e67e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+require "pry"
 
-def countdown
+def countdown(count)
+    count.downto(1) do | i |
+        print "#{i} SECOND(S)!\n"
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(timer)
+    if timer >= 5
+        sleep(timer)
+    end
+end
+
+# binding.pry
